b204ea3fd94282df2d1cecefdc5dd35f7afd47e2
sajalrasto <30717607+sajalrasto@users.noreply.github.com>
2022-08-30 17:08:34 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Hello Jhooq";
+        return "Hello guys, I am Sajal, welcome to Globallogic";
     }
 }
